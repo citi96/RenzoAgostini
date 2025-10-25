92e99a18db7f5522a5a8f480551905b93cf2cda3
Francesco Chiti <fchiti071@gmail.com>
2025-10-25 17:07:20 +0200

    MODIFICHE PER DEPLOY CLIENT

--- docker/client-entrypoint.sh
+++ docker/client-entrypoint.sh
@@ -8,7 +8,7 @@ if [ -f "$TEMPLATE" ]; then
   export KEYCLOAK__URL="${KEYCLOAK__URL:-https://auth.renzoagostini.it}"
   export KEYCLOAK__REALM="${KEYCLOAK__REALM:-RenzoAgostiniRealm}"
   export KEYCLOAK__CLIENT_ID="${KEYCLOAK__CLIENT_ID:-web-5e6da1c6-5531-4a9f-a800-397948b8ba5d}"
-  export KEYCLOAK__REDIRECT_URI="${KEYCLOAK__REDIRECT_URI:-https://www.renzoagostini.it/auth/callback"
+  export KEYCLOAK__REDIRECT_URI="${KEYCLOAK__REDIRECT_URI:-https://www.renzoagostini.it/auth/callback}"
   export KEYCLOAK__POST_LOGOUT_REDIRECT_URI="${KEYCLOAK__POST_LOGOUT_REDIRECT_URI:-https://www.renzoagostini.it/}"
   export BASE_URL="${BASE_URL:-https://api.renzoagostini.it}"
 
